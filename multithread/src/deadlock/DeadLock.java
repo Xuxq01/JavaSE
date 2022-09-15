e15092570344b73bad5fc27b0bf6307bea7bd684
Xuxq01 <103299608+Xuxq01@users.noreply.github.com>
2022-09-15 15:03:26 +0800

    Add files via upload

---
+++ multithread/src/deadlock/DeadLock.java
@@ -0,0 +1,57 @@
+package deadlock;
+/*
+死锁要会写
+ */
+public class DeadLock {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        Object o2 = new Object();
+
+        Thread t1 = new MyThread1(o1,o2);
+        Thread t2 = new MyThread1(o1,o2);
+    }
+}
+
+class MyThread1 extends Thread{
+    Object o1;
+    Object o2;
+    public MyThread1(Object o1,Object o2){
+        this.o1 = o1;
+        this.o2 = o2;
+    }
+
+    public void run(){
+        synchronized (o1){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (o2){
+
+            }
+        }
+    }
+}
+
+class MyThread2 extends Thread{
+    Object o1;
+    Object o2;
+    public MyThread2(Object o1,Object o2){
+        this.o1 = o1;
+        this.o2 = o2;
+    }
+
+    public void run(){
+        synchronized (o2){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (o1){
+
+            }
+        }
+    }
+}
