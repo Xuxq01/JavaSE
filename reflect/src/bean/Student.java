ca478f0db519a66b789a462a8ca817855fcb7c4d
Xuxq01 <103299608+Xuxq01@users.noreply.github.com>
2022-09-16 21:47:02 +0800

    Add files via upload

---
+++ reflect/src/bean/Student.java
@@ -0,0 +1,10 @@
+package bean;
+//反射属性Field
+public class Student {
+    //Field译为字段,其实就是属性/成员
+    public int no;
+    private String name;
+    protected int age;
+    boolean sex;
+    public static final double MATH_PI = 3.1415926;
+}
